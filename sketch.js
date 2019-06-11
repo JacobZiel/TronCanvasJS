e3572944a3afd95bf6a9f725f18f03cf698efda4
jacobzielinski <32741770+jacobzielinski@users.noreply.github.com>
2019-06-11 15:23:58 +0100

    first commit

---
+++ sketch.js
@@ -0,0 +1,147 @@
+var s;
+let p;
+var scl = 5;
+let arrowUp = true;
+let arrowDown = true;
+let arrowLeft = true;
+let arrowRight = true;
+let wUp = true;
+let sDown = true;
+let aLeft = true;
+let dRight = true;
+let wait = new Audio("./engine.wav");
+let drive = new Audio("./cycle.wav")
+let gameOver = new Audio("./gameover.wav")
+pointsBlue = 0;
+pointsRed = 0;
+drive.pause();
+let press = false;
+let start = true;
+let mainFont
+
+function preload() {
+  mainFont = loadFont('./font.ttf')
+}
+
+function setup() {
+  createCanvas(800, 500);
+  s = new Snake(00, 250, "red");
+  p = new Snake(800, 250, "green")
+  frameRate(30);
+  wait.play();
+}
+
+function mousePressed() {
+  // Check if mouse is inside the circle
+  let d = dist(mouseX, mouseY, 350, 200);
+  if (d < 100) {
+    press = true;
+    // Pick new random color values
+  }
+}
+
+function draw() {
+  background(21, 32, 51);
+  textFont(mainFont)
+  textSize(26)
+  fill("Red")
+  let title = text("Tron 2000", 275, 140);
+  fill("yellow")
+  textSize(16)
+  let startText = text("START", 350, 200);
+  fill("yellow")
+  if (press) {
+    // startText.clear();
+    // title.clear()
+    background(21, 32, 51);
+    start = false;
+    s.death();
+    s.update();
+    s.show();
+    p.death();
+    p.update();
+    p.show();
+    s.total++
+    p.total++
+    textSize(16);
+    fill("blue")
+    text(`P2: ${pointsBlue}`, 50, 30);
+    fill("yellow")
+    text(`P1: ${pointsRed}`, 650, 30);
+  }
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 38:
+      arrowDown = false;
+      arrowLeft = true;
+      arrowRight = true;
+      if (arrowUp) {
+        p.dir(0, -1);
+      }
+      break;
+    case 40:
+      arrowLeft = true;
+      arrowRight = true;
+      arrowUp = false;
+      if (arrowDown) {
+        p.dir(0, 1);
+      }
+      break;
+    case 39:
+      arrowUp = true;
+      arrowDown = true;
+      arrowLeft = false;
+      if (arrowRight) {
+
+        p.dir(1, 0);
+      }
+      break;
+    case 37:
+      arrowUp = true;
+      arrowDown = true;
+      arrowRight = false;
+      if (arrowLeft) {
+        p.dir(-1, 0);
+      }
+      break;
+    case 87:
+      sDown = false;
+      aLeft = true;
+      dRight = true;
+      if (wUp) {
+        s.dir(0, -1)
+      }
+      break;
+    case 83:
+      wUp = false;
+      aLeft = true;
+      dRight = true;
+      if (sDown) {
+        s.dir(0, 1)
+      }
+      break;
+    case 68:
+      aLeft = false;
+      wUp = true;
+      sDown = true;
+      if (dRight) {
+        s.dir(1, 0);
+      }
+      break;
+    case 65:
+      dRight = false;
+      wUp = true;
+      sDown = true;
+      if (aLeft) {
+        s.dir(-1, 0);
+      }
+      break;
+    case 13:   
+      this.setup()
+      break;
+    default:
+      break;
+  }
+}
---
+++ snake.js
@@ -0,0 +1,86 @@
+let reset = true;
+
+function Snake(x, y, color) {
+  this.x = x;
+  this.y = y;
+  this.xspeed = 0;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.eat = function (pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  this.dir = function (x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  this.death = function () {
+    for (var i = 0; i < this.tail.length; i++) {
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y)
+      let b = dist(s.x, s.y, pos.x, pos.y);
+      let g = dist(p.x, p.y, pos.x, pos.y);
+      if (d < 1) {
+        this.total = 0;
+        this.tail = [];
+        this.xspeed = 0
+        this.yspeed = 0
+
+      } else if (b < 1) {
+        s.total = 0;
+        s.tail = [];
+        s.xspeed = 0
+        s.yspeed = 0
+        pointsblue=1; //blue
+        setup()
+      } else if (g < 1) {
+        p.total = 0;
+        p.tail = [];
+        p.xspeed = 0
+        p.yspeed = 0
+        pointsRed=1 //red
+        setup()
+      } else if (p.total < 2) {
+        pointsBlue=1
+      } else if (s.total < 2) {
+        pointsRed=1
+      
+      }
+    }
+  }
+
+
+  this.update = function () {
+
+    if (this.total >= 1) {
+      this.tail[this.total - 1] = createVector(this.x, this.y);
+    }
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
+
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
+  }
+
+  this.show = function () {
+    fill(color);
+    stroke(0, 0, 0, 0)
+
+
+    // pixelDensity(1)
+    for (var i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+    rect(this.x, this.y, scl, scl);
+
+  }
+}
